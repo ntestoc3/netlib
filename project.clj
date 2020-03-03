f62f7eecf4028b729c04f2fb3ea2381e1960e56b
ntestoc3 <ntestoc3@outlook.com>
2020-03-03 17:11:12 +0800

    clojars部署

--- project.clj
+++ project.clj
@@ -3,26 +3,35 @@
   :url "https://github.com/ntestoc3/netlib"
   :license {:name "MIT"
             :url "https://opensource.org/licenses/MIT"}
+  :deploy-repositories [["releases" {:url "https://repo.clojars.org"
+                                     :username :env/clojars_user
+                                     :password :env/clojars_pass
+                                     }]
+                        ["snapshots" {:url "https://repo.clojars.org"
+                                      :username :env/clojars_user
+                                      :password :env/clojars_pass
+                                      }]]
+
   :dependencies [[org.clojure/clojure "1.10.1"]
                  [ntestoc3/common "2.0.0-SNAPSHOT"]
 
                  [buddy/buddy-core "1.6.0"]                    ; crypt
                  [camel-snake-kebab/camel-snake-kebab "0.4.1"] ;; name convert
-                 [cheshire/cheshire "5.10.0"]                   ;; json
+                 [cheshire/cheshire "5.10.0"]                  ;; json
                  [clj-http "3.10.0"]
                  [clojure.java-time "0.3.2"] ; datetime
                  [com.cemerick/url "0.1.1"]  ; uri java.net
 
-                 [com.taoensso/timbre "4.10.0"] ;; better logging
-                 [commons-net "3.6"] ;; whois client
+                 [com.taoensso/timbre "4.10.0"]   ;; better logging
+                 [commons-net "3.6"]              ;; whois client
                  [dnsjava/dnsjava "3.0.0-next.1"] ;; dns client
-                 [funcool/octet "1.1.2"]   ;; bytes buffer
-                 [me.raynes/fs "1.4.6"]            ;; file utility
+                 [funcool/octet "1.1.2"]          ;; bytes buffer
+                 [me.raynes/fs "1.4.6"]           ;; file utility
                  [org.clojure/data.zip "0.1.3"]
                  [org.slf4j/slf4j-simple "1.7.30"]
-                 [reaver/reaver "0.1.2"]              ;html parser
+                 [reaver/reaver "0.1.2"] ;html parser
 
-                ]
+                 ]
   :profiles {:dev {:dependencies [[midje "1.9.9" :exclusions [org.clojure/clojure]]
                                   ]
                    :plugins [[lein-midje "3.2.1"]]}}
